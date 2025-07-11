2fba55f87d4943b808bb981aeb8bf4852751b787
Secure Manager Research Team <ashkan.secure@gmail.com>
2025-07-11 10:40:16 +0330

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,11 @@
+import React from 'react';
 
+function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>سلام! این پروژه React + Vercel API است</h1>
+    </div>
+  );
+}
+
+export default App;
