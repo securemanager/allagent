86900e2d232ee97199d848dbddaed6166e18607d
Secure Manager Research Team <ashkan.secure@gmail.com>
2025-07-11 10:02:17 +0330

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,20 +2,18 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-// سرو کردن فایل‌های استاتیک ساخته شده React
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'سلام از سرور Express!' });
 });
 
-// همه درخواست‌های دیگر را به React برگردان
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
